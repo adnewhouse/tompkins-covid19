99dd62bc5aaf5647358b63cef5244e4675b48951
Adam Newhouse <adnewhouse@gmail.com>
2020-03-31 02:45:04 -0500

    Add some history

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import requests
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 import pandas as pd
 
 sns.set()
@@ -22,22 +23,33 @@ combined.insert(0, 'Date', [str(date.today())], True)
 history = pd.read_csv('history.csv', header=0, index_col=[0])
 
 new_history = pd.concat([history, combined], ignore_index=True)
-new_history.drop_duplicates(subset=['Positive Test Results'], keep='last', inplace=True)
+new_history.drop_duplicates(subset=['Date', 'Total Tested for COVID-19'], keep='first', inplace=True)
 new_history.reset_index(drop=True, inplace=True)
 new_history.to_csv('history.csv', mode='w', header=True)
 
 
 # Generate Plots
-history = pd.read_csv('history.csv', header=0, index_col=[0])
+history = pd.read_csv('history.csv', header=0, index_col=[0], parse_dates=['Date'])
 history.set_index('Date', inplace=True)
 
-fig = plt.figure(dpi=300)
-ax = history[['Positive Test Results']].plot.bar(title='Tompkins County Confirmed Covid-19 Cases', rot=45, ax=plt.gca())
-for p in ax.patches:
-    ax.annotate(str(p.get_height()), (p.get_x() + (p.get_width() / 2), p.get_height() * 1.005), ha='center', va='center', xytext=(0, 5), textcoords='offset points')
-fig.savefig('positive_cases.png', dpi=fig.dpi, bbox_inches='tight')
+fig, ax = plt.subplots()
+ax.plot(history.index, history['Positive Test Results'], 'o-')
+ax.set(
+        title='Tompkins County Confirmed Covid-19 Cases',
+        xlabel='Date',
+        ylabel='Cases')
+
+ax.xaxis.set_major_formatter(mdates.DateFormatter('%m/%d'))
+ax.tick_params(axis='x', rotation=60)
+fig.savefig('positive_cases.png', dpi=300, bbox_inches='tight')
 
 
+'''
 fig = plt.figure(dpi=300)
 history[['Positive Test Results', 'Negative Test Results', 'Total Tested for COVID-19']].plot.line(title='Tompkins County Covid-19 Tests', rot=45, ax=plt.gca())
 fig.savefig('tests.png', dpi=fig.dpi, bbox_inches='tight')
+
+ax = sns.lineplot(data=history)
+fig = ax.get_figure()
+fig.savefig("output.png")
+'''
